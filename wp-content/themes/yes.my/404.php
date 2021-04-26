88841c12bc1be29bede2dcac11cddcbf746e4a00
mynameislatif <al.latif87@gmail.com>
2021-04-26 23:08:30 +0800

    Added Content - Support - Coverage; Added redirection plugin; Added empty content page to show FAQ - Prepaid Add Ons content;

---
+++ wp-content/themes/yes.my/404.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<?php 
+    global $wp;
+
+    $request_url    = $wp->request;
+    $faq_post_id    = REDIRECT_FAQ_EN;
+    $lang           = get_bloginfo("language");
+
+    if ($lang == 'ms-MY') {
+        $faq_post_id    = REDIRECT_FAQ_BM;
+    } else if ($lang == 'zh-CN') {
+        $faq_post_id    = REDIRECT_FAQ_CH;
+    }
+
+    $faq_post       = get_post($faq_post_id);
+
+    $content        = $faq_post->post_content;
+    $content        = apply_filters('the_content', $content);
+    $content        = str_replace(']]>', ']]&gt;', $content);
+    echo $content;
+?>
+
+<?php get_footer(); ?>
--- wp-content/themes/yes.my/page.php
+++ wp-content/themes/yes.my/page.php
@@ -2,17 +2,38 @@
 
 <?php 
     if ( have_posts() ) {
-        while ( have_posts() ) :
-            the_post();
-            $custom_code_css    = get_post_meta($post->ID, 'yesmy_custom_css', true);
-            $custom_code_js     = get_post_meta($post->ID, 'yesmy_custom_js', true);
-    
-            if ($custom_code_css) echo $custom_code_css;
-    
-            the_content();
-    
-            if ($custom_code_js) echo $custom_code_js;
-        endwhile;
+        $content    = get_the_content();
+
+        if (trim($content) == '') {
+            $request_url    = $wp->request;
+            $faq_post_id    = REDIRECT_FAQ_EN;
+            $lang           = get_bloginfo("language");
+
+            if ($lang == 'ms-MY') {
+                $faq_post_id    = REDIRECT_FAQ_BM;
+            } else if ($lang == 'zh-CN') {
+                $faq_post_id    = REDIRECT_FAQ_CH;
+            }
+
+            $faq_post       = get_post($faq_post_id);
+
+            $content        = $faq_post->post_content;
+            $content        = apply_filters('the_content', $content);
+            $content        = str_replace(']]>', ']]&gt;', $content);
+            echo $content;
+        } else {
+            while ( have_posts() ) :
+                the_post();
+                $custom_code_css    = get_post_meta($post->ID, 'yesmy_custom_css', true);
+                $custom_code_js     = get_post_meta($post->ID, 'yesmy_custom_js', true);
+        
+                if ($custom_code_css) echo $custom_code_css;
+        
+                the_content();
+        
+                if ($custom_code_js) echo $custom_code_js;
+            endwhile;
+        }
     }
 ?>
 
